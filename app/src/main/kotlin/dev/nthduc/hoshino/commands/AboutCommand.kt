cf97b6a4c51619e3689df4e59e8a41f8e28419d4
nthduc <nguyenthaiduc0212@gmail.com>
2023-07-10 21:19:33 +0700

    :hammer: add AboutCommand

---
+++ app/src/main/kotlin/dev/nthduc/hoshino/commands/AboutCommand.kt
@@ -0,0 +1,51 @@
+package dev.nthduc.hoshino.commands
+
+import dev.kord.common.Color
+import dev.kord.core.event.message.MessageCreateEvent
+import dev.kord.rest.builder.message.create.embed
+import dev.nthduc.hoshino.utils.getOwnerInfo
+import io.ktor.client.request.forms.*
+import io.ktor.utils.io.jvm.javaio.*
+import kotlinx.datetime.Clock
+import java.io.File
+import java.io.FileInputStream
+import java.net.URL
+
+class AboutCommand() : Command {
+    override val description: String
+        get() = "Hiển thị thông tin về bot"
+
+    override suspend fun execute(event: MessageCreateEvent) {
+        val message = event.message
+        val (userOwner, avatarOwner) = getOwnerInfo(event)
+        var url = this::class.java.getResource("/images/ai-hoshino.gif")
+        if (url != null) {
+            val fileInputStream = FileInputStream(File(url.toURI()))
+            val byteReadChannel = fileInputStream.toByteReadChannel()
+            val channelProvider = ChannelProvider(size = File(url.toURI()).length()) { byteReadChannel }
+            event.kord.rest.channel.createMessage(event.message.channelId) {
+                addFile("ai-hoshino.gif", channelProvider)
+                embed {
+                    title = "AI Hoshino"
+                    color = Color(49, 14, 76)
+                    description = """
+                    Ai Hoshino là một nhân vật chính trong bộ truyện tranh Oshi no Ko. Cô là người mẫu ảnh của loạt phim và từng là thần tượng hàng đầu của [B-Komachi](https://oshinoko.fandom.com/wiki/B-Komachi). 
+                    
+                    Bot sử dụng [Java](https://www.java.com/en/) và [Kotlin](https://kotlinlang.org/) để tương tác với Discord API.
+                """.trimIndent()
+                    thumbnail {
+                        url = URL("attachment://ai-hoshino.gif")
+                    }
+                    footer {
+                        text = "Bot được phát triển bởi $userOwner"
+                        icon = "$avatarOwner"
+                    }
+                    timestamp = Clock.System.now()
+                }
+            }
+        } else {
+            message.channel.createMessage("Lệnh about đang gặp vấn đề , hãy chờ fix nhé ^_^")
+        }
+    }
+
+}
